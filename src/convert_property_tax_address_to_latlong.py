57901c1a2e590e5f9ef670ebf5aea5238e5aa41e
Walther Maciel <walthermaciel@gmail.com>
2016-03-10 13:36:11 -0800

    Code to convert adress to lat long

--- src/convert_property_tax_address_to_latlong.py
+++ src/convert_property_tax_address_to_latlong.py
@@ -1,78 +1,71 @@
 __author__ = 'walthermaciel'
 
 from geopy.geocoders import DataBC
+from geopy.exc import GeopyError
 from time import sleep
 import sys
+import csv
+import random
 
-def main(y):
+
+def main(path_in):
     geolocator = DataBC()
+    print 'loading', path_in
+    fpin = open(path_in, 'r')
 
-    print 'starting', y
-    fpin = open('../data/property_tax_06_15/property_tax_report_csv' + str(y) + '.csv', 'r')
-    fpout = open('../data/property_tax_06_15/clean_property_tax_'+ str(y) + '.csv', 'w')
+    reader = csv.DictReader(fpin)
+    d_list = []
+    for row in reader:
+        d = dict(STREET_NAME=row['STREET_NAME'],
+                 STREET_NUMBER=row['STREET_NUMBER'],
+                 YEAR=row['YEAR'],
+                 VALUE=row['VALUE'])
 
-    i_st_name = -1
-    i_st_num = -1
-    i_curr_value = -1
-    i_pid = -1
+        if d['STREET_NUMBER'].endswith('.0'):
+            d['STREET_NUMBER'] = d['STREET_NUMBER'][:-2]
 
-    for i, line in enumerate(fpin):
-        cols = line.replace(', ', ' ').strip().split(',')
+        if d['YEAR'].endswith('.0'):
+            d['YEAR'] = d['YEAR'][:-2]
 
-        if i == 0:
-            for i_c, c in enumerate(cols):
-                if c == 'STREET_NAME':
-                    i_st_name = i_c
-                elif c == 'TO_CIVIC_NUMBER':
-                    i_st_num = i_c
-                elif c == 'PID':
-                    i_pid = i_c
-                elif c == 'CURRENT_LAND_VALUE':
-                    i_curr_value = i_c
-            fpout.write('YEAR,PID,LAND_VALUE,LAT,LONG\n')
+        d_list.append(d)
 
-        else:
-            # Check if it has PID
-            pid = cols[i_pid]
-            if pid == '':
-                continue
+    y = str(int(d_list[1]['YEAR']))
+    fpout = open('../data/property_tax_06_15/latlong_property_tax_'+ str(y) + '.csv', 'w')
+    fpout.write('YEAR,VALUE,STREET_NUMBER,STREET_NAME,LATITUDE,LONGITUDE\n')
 
-            # Check if it has street name
-            st_name = cols[i_st_name]
-            if st_name == '':
-                continue
+    random.shuffle(d_list)
+    n_lines = float(len(d_list))
+    curr_line = 0
+    for d in d_list:
+        curr_line += 1
+        st_num = str(int(d['STREET_NUMBER']))
+        st_name = str(d['STREET_NAME'])
+        address = st_num + ' ' + st_name + ' Vancouver BC Canada'
 
-            # Check if it has street number
-            st_num = cols[i_st_num]
-            if st_num == '':
-                continue
+        got_it = False
+        delay = 1
+        while not got_it:
+            try:
+                sleep(1)
+                location = geolocator.geocode(address)
+                got_it = True
+            except GeopyError:
+                delay *= 2
+                got_it = False
+                print 'try again...'
 
-            # Check if it has current land value
-            curr_value = cols[i_curr_value]
-            if curr_value == '':
-                continue
+        latitude = "{:.15f}".format(location.latitude)
+        longitude = "{:.15f}".format(location.longitude)
+        curr_value = str(float(d['VALUE']))
 
-            address = st_num + ' ' + st_name + ' Vancouver BC Canada'
+        fpout.write(str(y) + ',' + curr_value + ',' + st_num + ',' + st_name + ',' + latitude + ',' + longitude + '\n')
+        print str(100 * curr_line/n_lines) + '%, ' + str(y) + ',' + curr_value + ',' + latitude + ',' + longitude + ', '+ st_num, st_name
 
-            found_it = False
-            while not found_it:
-                try:
-                    location = geolocator.geocode(address)
-                    sleep(0.5)
-                    found_it = True
-                except:
-                    found_it = False
-
-            latitude = str(location.latitude)
-            longitude = str(location.longitude)
-
-            fpout.write(str(y) + ',' + pid + ',' + curr_value + ',' + latitude + ',' + longitude + '\n')
-            print str(y) + ',' + pid + ',' + curr_value + ',' + latitude + ',' + longitude + ', '+ st_num, st_name
-
-        fpin.close()
-        fpout.close()
+    fpin.close()
+    fpout.close()
 
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    for path in sys.argv[1:]:
+        main(path)
