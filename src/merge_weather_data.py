a0c1499bb7cfe076baa5d538c3297d5d8f5cbd58
jdsarr <jdsarr@gmail.com>
2016-03-11 00:03:15 -0800

    Merge summary weather data by month and year

---
+++ src/merge_weather_data.py
@@ -0,0 +1,29 @@
+import csv, pandas as pd, numpy as np
+
+def main(pr):
+
+    dfs = []
+
+    for i in xrange(2006,2016,1):
+        df = pd.read_csv(pr+'data/weather/VANCOUVER HARBOUR CS/'+ str(i)  +'data.csv')
+
+        df = df.loc[:,
+             ['Year', 'Month', 'Day', 'Max Temp (\xb0C)', 'Min Temp (\xb0C)', 'Mean Temp (\xb0C)', 'Total Precip (mm)']]
+
+
+        dfs.append(df)
+
+
+    weather_data = pd.concat(dfs)
+    weather_data = weather_data.groupby(['Year','Month']).agg({'Max Temp (\xb0C)': np.mean,
+                                                               'Min Temp (\xb0C)': np.mean,
+                                                               'Mean Temp (\xb0C)': np.mean,
+                                                               'Total Precip (mm)': np.sum})
+
+    print weather_data
+    weather_data.to_csv(pr+'data/weather/VANCOUVER HARBOUR CS/summarydata.csv')
+
+if __name__ == "__main__":
+
+    project_root = '../'
+    main( project_root)
