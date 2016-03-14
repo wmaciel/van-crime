2df3a479a23a6e10e90139fbccd110adb644ff7f
wmaciel <walthermaciel@gmail.com>
2016-03-14 11:00:39 -0700

    Smarter version that creates a local database of the known addresses!

--- src/convert_property_tax_address_to_latlong.py
+++ src/convert_property_tax_address_to_latlong.py
@@ -7,7 +7,9 @@ import sys
 import csv
 import random
 from ssl import SSLError
+import os.path
 
+local_db = {}
 
 def main(path_in):
     geolocator = DataBC()
@@ -41,32 +43,55 @@ def main(path_in):
         curr_line += 1
         st_num = str(int(d['STREET_NUMBER']))
         st_name = str(d['STREET_NAME'])
-        address = st_num + ' ' + st_name + ' Vancouver BC Canada'
+        
+        if (st_name, st_num) in local_db:
+            latitude, longitude = local_db[(st_name, st_num)]
+        else:
+            print 'querying...'      
+            address = st_num + ' ' + st_name + ' Vancouver BC Canada'
 
-        got_it = False
-        delay = 1
-        while not got_it:
-            try:
-                sleep(1)
-                location = geolocator.geocode(address)
-                got_it = True
-            except (GeopyError, SSLError) as e:
-                delay *= 2
-                got_it = False
-                print 'try again...'
+            got_it = False
+            delay = 1
+            while not got_it:
+                try:
+                    sleep(1)
+                    location = geolocator.geocode(address)
+                    got_it = True
+                except (GeopyError, SSLError) as e:
+                    delay *= 2
+                    got_it = False
+                    print 'try again...'
 
-        latitude = "{:.15f}".format(location.latitude)
-        longitude = "{:.15f}".format(location.longitude)
+            latitude = "{:.15f}".format(location.latitude)
+            longitude = "{:.15f}".format(location.longitude)
+        
         curr_value = str(float(d['VALUE']))
 
         fpout.write(str(y) + ',' + curr_value + ',' + st_num + ',' + st_name + ',' + latitude + ',' + longitude + '\n')
         print str(100 * curr_line/n_lines) + '%, ' + str(y) + ',' + curr_value + ',' + latitude + ',' + longitude + ', '+ st_num, st_name
+        local_db[(st_name, st_num)] = (latitude, longitude)
 
     fpin.close()
     fpout.close()
 
 
+def load_local_db():
+    for y in xrange(2006, 2016):
+        path = '../data/property_tax_06_15/latlong_property_tax_'+ str(y) + '.csv'
+        if os.path.isfile(path):
+            print 'loading', path
+            fp = open(path, 'r')
+            for line in fp:
+                tokens = line.strip().split(',')
+                st_num = tokens[2]
+                st_name = tokens[3]
+                lat = tokens[4]
+                lon = tokens[5]
+                if (st_name, st_num) not in local_db:
+                    local_db[(st_name, st_num)] = (lat, lon)
+
 
 if __name__ == '__main__':
+    load_local_db()
     for path in sys.argv[1:]:
         main(path)
